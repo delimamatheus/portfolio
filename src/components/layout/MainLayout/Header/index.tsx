d39d03da956d953ce81ae62a9d60bdf916605602
delimamatheus <matheus.lima@unigran.br>
2023-01-31 15:53:47 -0300

    ✨ add `Header`

--- src/components/layout/MainLayout/Header/index.tsx
+++ src/components/layout/MainLayout/Header/index.tsx
@@ -1,13 +1,22 @@
 import { Flex, Box, Center, Text } from '@chakra-ui/layout'
+import { BoxProps } from '@chakra-ui/react';
+import { ReactNode } from 'react';
 
-export function Header() {
+interface HeaderProps extends BoxProps {
+  children: ReactNode;
+}
+
+export function Header( { children }: HeaderProps) {
   return (
-    <Box as='header' boxShadow='lg'>
-      <Center bgColor='cinza.700' h='50px'>
-        <Flex w='100%' justifyContent='center'>
-            <Text fontSize={'36px'}>HEADER</Text>
-        </Flex>
-      </Center>
-    </Box>
+    <Center as='header'>
+      <Box w='90%'>
+        <Center bgColor='black' boxShadow='lg' borderBottomRadius='10px' opacity='40%' h='80px'>
+          <Flex w='100%' alignItems={'center'}>
+              {/* <Text fontSize={'36px'}>HEADER</Text> */}
+              {children}
+          </Flex>
+        </Center>
+      </Box>
+    </Center>
   )
 }
--- src/layout/MainLayout/index.tsx
+++ src/layout/MainLayout/index.tsx
@@ -3,8 +3,9 @@ import { ReactNode } from "react";
 import { Content } from "../../components/layout/MainLayout/Content";
 import { Footer } from "../../components/layout/MainLayout/Footer";
 import { Header } from "../../components/layout/MainLayout/Header";
-import { Flex } from "@chakra-ui/layout";
-
+import { Box, Center, Flex, Text } from "@chakra-ui/layout";
+import { Icon } from "@chakra-ui/react";
+import { FaLinkedin, FaGithubSquare, FaYoutubeSquare, FaInstagramSquare } from "react-icons/fa"
 interface MainLayoutProps {
   children?: ReactNode | ReactNode[] | string;
 }
@@ -12,7 +13,53 @@ interface MainLayoutProps {
 export function MainLayout({ children }: MainLayoutProps) {
   return (
     <>    
-        <Header />
+        <Header>                    
+          {/* LOGO */}
+
+          <Box w='20%' text-align='center'>
+            <Box as='button'>
+              <Text fontSize={'36px'} color='white'>LOGO</Text>
+            </Box>
+          </Box>
+
+          {/* Buttons */}
+
+          <Box w='60%' display='flex' flexDirection='row' justifyContent='center'>
+            <Box as='button' w='120px'>
+              <Text color='white' fontSize={'20px'} textAlign='center'>Início</Text>
+            </Box>
+            <Box as='button' w='120px'>
+              <Text color='white' fontSize={'20px'} textAlign='center'>Sobre Mim</Text>
+            </Box>
+            <Box as='button' w='120px'>
+              <Text color='white' fontSize={'20px'} textAlign='center'>Serviços</Text>
+            </Box>
+            <Box as='button' w='120px'>
+              <Text color='white' fontSize={'20px'} textAlign='center'>Portfólio</Text>
+            </Box>
+            <Box as='button' w='120px'>
+              <Text color='white' fontSize={'20px'} textAlign='center'>Contato</Text>
+            </Box>
+          </Box>
+
+          {/* Social Medias */}
+
+          <Box w='20%' display='flex' flexDirection='row' justifyContent='center'>
+            <Box>
+              <Icon color='white' as={FaLinkedin} />
+            </Box>
+            <Box>
+              <Icon color='white' as={FaGithubSquare} />
+            </Box>
+            <Box>
+              <Icon color='white' as={FaInstagramSquare} />
+            </Box>
+            <Box>
+              <Icon color='white' as={FaYoutubeSquare} />
+            </Box>
+          </Box>
+
+        </Header>
 
         <Content>{children}</Content>
 
